601cd3fe30ea33d5bc3e706eb7f63b4fd67c1f3d
ktmurray1999 <43282256+ktmurray1999@users.noreply.github.com>
2020-12-05 12:02:52 -0500

    Add Discrimination Task
    Added the new discrimination task file and retina v_4 and v_5 that are built to perform on task.

--- support_discrim.py
+++ support_discrim.py
@@ -6,6 +6,7 @@ Created on Fri Nov 27 16:19:56 2020
 """
 import numpy as np
 from celluloid import Camera
+import seaborn as sns
 import matplotlib.pyplot as plt
 import torch
 dtype = torch.float
@@ -13,7 +14,7 @@ device = torch.device("cuda:0")
 
 
 class moveObjs():
-    def __init__(self, dims, direction, initial_pos, unique=False):
+    def __init__(self, dims, angle, initial_pos, unique=False):
         '''
         Initialization.
         INPUT:
@@ -22,7 +23,7 @@ class moveObjs():
             - unique: is this object the unique one?
         '''
         self.dims = dims
-        self.direction = direction
+        self.angle = angle
         self.initial_x = initial_pos[0]
         self.initial_y = initial_pos[1]
         self.x_coords = [initial_pos[0],]
@@ -33,15 +34,12 @@ class moveObjs():
         '''
         Create forward motion for the object one step at a time.
         '''
-        if self.unique and (self.x_coords[-1] != None or self.y_coords[-1] != None):
-            x_step = self.x_coords[-1] - self.direction[0]
-            y_step = self.y_coords[-1] - self.direction[1]
-        elif self.x_coords[-1] == None or self.y_coords[-1] == None:
+        if self.x_coords[-1] == None or self.y_coords[-1] == None:
             x_step = -1
             y_step = -1
         else:
-            x_step = self.x_coords[-1] + self.direction[0]
-            y_step = self.y_coords[-1] + self.direction[1]
+            x_step = self.x_coords[-1] + np.cos(self.angle)
+            y_step = self.y_coords[-1] + np.sin(self.angle)
         if x_step < 0 or y_step < 0 or x_step >= self.dims[0] or y_step >= self.dims[0]:
             self.x_coords.append(None)
             self.y_coords.append(None)
@@ -49,7 +47,7 @@ class moveObjs():
         else:
             self.x_coords.append(x_step)
             self.y_coords.append(y_step)
-            return (x_step, y_step)
+            return (int(x_step), int(y_step))
     
     def getHistory(self,):
         '''
@@ -61,7 +59,7 @@ class moveObjs():
 
 
 class scene():
-    def __init__(self, pixels, frames, num_objects, num_unique, r_discrim=1):
+    def __init__(self, pixels, frames, num_objects, perct_unique, direc_var, r_discrim):
         '''
         Initialization.
         INPUT:
@@ -72,20 +70,23 @@ class scene():
             - r_discrim: the rate of discrimination in the unique direction
                 - value between 0 and 1, usually assign 1
         '''
-        self.dims = (pixels,pixels,frames)
+        self.dims = (pixels*3,pixels*3,frames)
+        self.pixels = pixels
         self.stack = np.zeros(self.dims)
         self.num_objects = num_objects
-        self.num_unique = num_unique
-        self.radius = int(frames/20)
+        self.num_unique = int(num_objects*perct_unique)
+        self.radius = int(pixels/50)
         self.objs = []
-        self.index_unique = np.random.choice(num_objects, num_unique, replace=False).tolist()
+        self.index_unique = np.random.choice(num_objects, self.num_unique, replace=False).tolist()
+        self.initial_ang = np.random.random_sample()*2*np.pi
+        self.initial_un_ang = self.initial_ang - np.pi*r_discrim
+        self.variance = direc_var*2*np.pi
         
     def findPoints(self, point, fr):
         '''
         Given a point, return all the points in the circle around the point.
-            ( x - h )^2 + ( y - k )^2 = r^2
+            ( x - h )^2 + ( y - k )^2 = r^2 
         '''
-        list_points = []
         for x in range(point[0]-self.radius-1,point[0]+self.radius+2):
             for y in range(point[1]-self.radius-1,point[1]+self.radius+2):
                 if (x - point[0])**2 + (y - point[1])**2 <= self.radius**2 and not (x < 0 or y < 0 or x >= self.dims[0] or y >= self.dims[1]):
@@ -95,22 +96,16 @@ class scene():
         '''
         Create the objects for the scene.
         '''
-        direc_dist = np.random.randint(-3, high=4, size=2)
-        if direc_dist[0] == 0 and direc_dist[1] == 0:
-            print('ERR')
-            direction = (0,1)
-        else:
-            direction = (direc_dist[0],direc_dist[1])
-        print(direction)
         xChoice = np.random.choice(self.dims[0], self.num_objects, replace=False)
         yChoice = np.random.choice(self.dims[0], self.num_objects, replace=False)
         initial_positions = [(xChoice[p], yChoice[p]) for p in range(len(xChoice))]
         for p in range(self.num_objects):
             self.findPoints(initial_positions[p], 0)
+            angleVariance = 2*self.variance*np.random.random_sample() - self.variance
             if p in self.index_unique:
-                self.objs.append(moveObjs(self.dims,direction,initial_positions[p], unique=True))
+                self.objs.append(moveObjs(self.dims, self.initial_un_ang + angleVariance, initial_positions[p], unique=True))
             else:
-                self.objs.append(moveObjs(self.dims,direction,initial_positions[p]))
+                self.objs.append(moveObjs(self.dims, self.initial_ang + angleVariance, initial_positions[p]))
     
     def moveObjs(self,):
         '''
@@ -128,20 +123,127 @@ class scene():
         '''
         self.createObjs()
         self.moveObjs()
-        return self.stack
+        return self.stack[self.pixels:2*self.pixels,self.pixels:2*self.pixels,:], self.initial_un_ang
+
+
+def mse(t1, t2):
+    diff = t1 - t2
+    return torch.sum(diff * diff) / diff.numel() # + lambda*recreate
+
+
+def environment(rounds, pixels, frames, num_objects, perct_unique, direc_var, r_discrim):
+    '''
+    Function environment creates the set of stimuli specified by the parameters given.
+    INPUTS:
+        rounds - number of stimuli to include
+        pixels - number of pixels to include in the horrizontal and vertical direction
+        frames - number of frames to include in each stimuli draw
+        num_objects - number of objects to include in total
+        perct_unique - percent of the objects that go in the nonstandard direction
+        direc_var - variance in the direction from 0 to 1
+        r_discrim - the amount of discrimination in the standard and nonstandard direction (0 - 1)
+    '''
+    stimuli = []
+    cent_loc = []
+    for i in range(rounds):
+        scene1, cent_loc1 = scene(pixels, frames, num_objects, perct_unique, direc_var, r_discrim).createScene()
+        stimuli.append(scene1)
+        cent_loc.append(cent_loc1)
+    # for i,e in enumerate(cent_loc):
+    #     cent_loc[i] = e - 50*np.ones((len(e[:,0]), len(e[0,:])))
+        
+    envi = None
+    for i, e in enumerate(stimuli):
+        hold = np.moveaxis(e, -1, 0)
+        hold = torch.from_numpy(hold).to(device).float()
+        hold = torch.unsqueeze(hold,0)
+        hold = torch.unsqueeze(hold,0)
+        if envi is None:
+            envi = hold
+        else:
+            envi = torch.cat((envi, hold),0)
+    envi.requires_grad_(True)
+    res = None
+    for i, e in enumerate(cent_loc):
+        hold = np.array(e)
+        hold = torch.from_numpy(hold).to(device).float()
+        hold = torch.unsqueeze(hold, 0)
+        if res is None:
+            res = hold
+        else:
+            res = torch.cat((res, hold),0)
+    res.requires_grad_(True)
+    return envi, res
+
+
+def optimize_func(scence, cent_loc, net, num_iter):
+    net.train()
+    optimizer = torch.optim.Adam(net.parameters())
+    x = []
     
+    for i in range(num_iter):
+        optimizer.zero_grad()
+        output = net(scence)
+        loss = mse(output, cent_loc)
+        x.append(loss.item())
+        loss.backward()
+        optimizer.step()
+    return x, loss, output, net
+
+
+def createTest(net, pixels, frames, num_objects, perct_unique, direc_var, r_discrim):
+    envi, res = environment(2, pixels, frames, num_objects, perct_unique, direc_var, r_discrim)
+    loss_vals, loss, pred_py, net = optimize_func(envi, res, net, 1)
+    return loss
+
+
+def create_summary_apriori(ret):
+    fig, axs = plt.subplots(2, 2)
+    axs[1,0].plot(torch.squeeze(ret.temporal_conv.weight).detach().cpu().numpy())
+    axs[1,1] = sns.heatmap(torch.squeeze(ret.space_conv.weight).detach().cpu().numpy())
+    axs[0,0].plot(torch.squeeze(ret.amacrine_kernel.weight).detach().cpu().numpy())
+    axs[0,1].plot(torch.squeeze(ret.ganglion_kernel.weight).detach().cpu().numpy())
+    axs[1,0].set_title('Bipolar Time Kernel')
+    axs[1,1].set_title('Bipolar Space Kernel')
+    axs[0,0].set_title('Amacrine Time Kernel')
+    axs[0,1].set_title('Ganglion Time Kernel')
+    axs[0,0].xaxis.set_ticklabels([])
+    axs[0,1].xaxis.set_ticklabels([])
+    plt.savefig('Q:/Documents/TDS SuperUROP/kernels.png')
+
+
+def make_kernel(alpha):
+    ray = torch.arange(1,102,1,device=device).float()
+    ker = alpha[0]*torch.cos(alpha[1]*torch.log(alpha[2]*ray+torch.abs(alpha[3])))+\
+    alpha[4]*torch.ones(101,device=device).float()
+    return torch.flip(ker, (0,)).detach().cpu().numpy()
+
+
+def create_summary_prior(ret):
+    fig, axs = plt.subplots(2, 2)
+    axs[1,0].plot(make_kernel(ret.temporal_conv.alpha)) # temporal_conv
+    axs[1,1] = sns.heatmap(torch.squeeze(ret.space_conv.weight).detach().cpu().numpy())
+    axs[0,0].plot(make_kernel(ret.amacrine_kernel.alpha)) # amacrine_kernel
+    axs[0,1].plot(make_kernel(ret.ganglion_kernel.alpha)) # ganglion_kernel
+    axs[1,0].set_title('Bipolar Time Kernel')
+    axs[1,1].set_title('Bipolar Space Kernel')
+    axs[0,0].set_title('Amacrine Time Kernel')
+    axs[0,1].set_title('Ganglion Time Kernel')
+    axs[0,0].xaxis.set_ticklabels([])
+    axs[0,1].xaxis.set_ticklabels([])
+    plt.savefig('Q:/Documents/TDS SuperUROP/kernels.png')
+
     
 if __name__ == "__main__":
-    test = scene(150,30,60,1)
-    testScene = test.createScene()
+    test = scene(250,200,240,0.05,0.05,0.75)
+    testScene, another = test.createScene()
     fig = plt.figure()
     camera = Camera(fig)
     for i in range(len(testScene[0,0,:])):
         plt.imshow(testScene[:,:,i], cmap='hot', interpolation='nearest')
         camera.snap()
     animation = camera.animate()
-    animation.save('testScene.gif', writer = 'pillow', fps=5)
-
+    animation.save('testScene.gif', writer = 'pillow', fps=30)
 
 
 
